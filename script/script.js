eb95cc180decd510495273a65577ed6e7fdcce6a
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-07-28 15:42:51 -0300

    teste

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
-
-function alteraImagem() {
-    var windowWidth = window.innerWidth;
-    if (windowWidth <= 800) {
-        document.getElementById("image").src ="images/image-header-mobile.jpg";
-    } else {
-        document.getElementById("image").src ="images/image-header-desktop.jpg";
-    } 
-};
-alteraImagem();
+// convém estar no onload da janela
+window.onload = function()
+{
+  var largura = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var img = document.querySelector("#image");
+  if(largura>=1024)
+  {
+    img.setAttribute('src', 'images/image-header-desktop.jpg');
+  
+  } else {
+    img.setAttribute('src', 'images/image-header-mobile.jpg');
+   
+  }
+} // fim do window.onload
